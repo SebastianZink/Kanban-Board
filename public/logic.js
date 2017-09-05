a522a0113f4514090cdd672789b966c58c00db8f
TEC\SZI <sebastian.zink@tecalliance.net>
2017-09-05 07:59:37 +0200

    comment to index.html -changed some settings for the view
    comment to logic.js
    -add new kanban for each bug/backlog

--- public/logic.js
+++ public/logic.js
@@ -1,49 +1,52 @@
 function createKanban() {
     setTimeout(function () {
         var time = document.getElementById('time').innerHTML;
-        document.getElementById('kanban').remove();
-        var kanban = document.createElement("div");
-        kanban.setAttribute('id', 'kanban');
-        document.body.appendChild(kanban);
+        document.getElementById('container').remove();
         var users = JSON.parse(document.getElementById('users').innerHTML);
         var backlogs = JSON.parse(document.getElementById('backlogs').innerHTML);
         var tasks = JSON.parse(document.getElementById('tasks').innerHTML);
-        var fields = [{
-                name: "id",
-                type: "string"
-            },
-            {
-                name: "status",
-                map: "state",
-                type: "string"
-            },
-            {
-                name: "text",
-                map: "label",
-                type: "string"
-            },
-            {
-                name: "tags",
-                type: "string"
-            },
-            {
-                name: "color",
-                map: "hex",
-                type: "string"
-            },
-            {
-                name: "resourceId",
-                type: "number"
-            }
-        ];
-
-        var source = {
-            localData: [],
-            dataType: "array",
-            dataFields: fields
-        };
-
+        var container = document.createElement("div");
+        container.setAttribute('id', 'container');
+        document.body.appendChild(container);
         for (var i = 0; i < backlogs.length; i++) {
+            var kanban = document.createElement("div");
+            kanban.setAttribute('id', 'kanban_' + backlogs[i]._id);
+            document.getElementById('container').appendChild(kanban);
+            var fields = [{
+                    name: "id",
+                    type: "string"
+                },
+                {
+                    name: "status",
+                    map: "state",
+                    type: "string"
+                },
+                {
+                    name: "text",
+                    map: "label",
+                    type: "string"
+                },
+                {
+                    name: "tags",
+                    type: "string"
+                },
+                {
+                    name: "color",
+                    map: "hex",
+                    type: "string"
+                },
+                {
+                    name: "resourceId",
+                    type: "number"
+                }
+            ];
+
+            var source = {
+                localData: [],
+                dataType: "array",
+                dataFields: fields
+            };
+
             var newObj = {
                 id: backlogs[i]._id,
                 state: "backlog",
@@ -53,76 +56,76 @@ function createKanban() {
                 resourceId: backlogs[i].user
             };
             source.localData.push(newObj);
-        }
 
-        for (var i = 0; i < tasks.length; i++) {
-            var newObj = {
-                id: tasks[i]._id,
-                state: "new",
-                label: tasks[i].label,
-                resourceId: tasks[i].user,
-                hex: "#0052f7"
-            };
-            source.localData.push(newObj);
-        }
+            for (var k = 0; k < tasks.length; k++) {
+                var newObj = {
+                    id: tasks[k]._id,
+                    state: "new",
+                    label: tasks[k].label,
+                    resourceId: tasks[k].user,
+                    hex: "#0052f7"
+                };
+                source.localData.push(newObj);
+            }
 
-        var dataAdapter = new $.jqx.dataAdapter(source);
-        var resourcesAdapterFunc = function () {
-            var resourcesSource = {
-                localData: [],
-                dataType: "array",
-                dataFields: [{
-                        name: "id",
-                        type: "number"
+            var dataAdapter = new $.jqx.dataAdapter(source);
+            var resourcesAdapterFunc = function () {
+                var resourcesSource = {
+                    localData: [],
+                    dataType: "array",
+                    dataFields: [{
+                            name: "id",
+                            type: "number"
+                        },
+                        {
+                            name: "name",
+                            type: "string"
+                        },
+                        {
+                            name: "image",
+                            type: "string"
+                        },
+                        {
+                            name: "common",
+                            type: "boolean"
+                        }
+                    ]
+                };
+
+                for (var p = 0; p < users.length; p++) {
+                    var newObj = {
+                        id: users[p].id,
+                        name: users[p].name,
+                        image: "../../images/Anne.png"
+                    };
+                    resourcesSource.localData.push(newObj);
+                };
+
+                var resourcesDataAdapter = new $.jqx.dataAdapter(resourcesSource);
+                return resourcesDataAdapter;
+            }
+            $('#kanban_' + backlogs[i]._id).jqxKanban({
+                resources: resourcesAdapterFunc(),
+                source: dataAdapter,
+                columns: [{
+                        text: "Backlog",
+                        dataField: "backlog"
                     },
                     {
-                        name: "name",
-                        type: "string"
+                        text: "New",
+                        dataField: "new"
                     },
                     {
-                        name: "image",
-                        type: "string"
+                        text: "In Progress",
+                        dataField: "work"
                     },
                     {
-                        name: "common",
-                        type: "boolean"
+                        text: "Done",
+                        dataField: "done"
                     }
                 ]
-            };
-
-            for (var i = 0; i < users.length; i++) {
-                var newObj = {
-                    id: users[i].id,
-                    name: users[i].name,
-                    image: "../../images/Anne.png"
-                };
-                resourcesSource.localData.push(newObj);
-            };
-
-            var resourcesDataAdapter = new $.jqx.dataAdapter(resourcesSource);
-            return resourcesDataAdapter;
+            });
+            $("#kanban-column-0").append("<button type='button'id='secondbutton'class='btn btn-info btn-lg' data-toggle='modal' data-target='#exampleModalLabel'>New task</button>");
         }
-        $('#kanban').jqxKanban({
-            resources: resourcesAdapterFunc(),
-            source: dataAdapter,
-            columns: [{
-                    text: "Backlog",
-                    dataField: "backlog"
-                },
-                {
-                    text: "New",
-                    dataField: "new"
-                },
-                {
-                    text: "In Progress",
-                    dataField: "work"
-                },
-                {
-                    text: "Done",
-                    dataField: "done"
-                }
-            ]
-        });
-        $("#kanban-column-0").append("<button type='button'id='secondbutton'class='btn btn-info btn-lg' data-toggle='modal' data-target='#exampleModalLabel'>New task</button>");
     }, Number(document.getElementById('time').innerHTML) + 700);
 }
